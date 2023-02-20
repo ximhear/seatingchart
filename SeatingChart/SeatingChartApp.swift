449ae93f39e13255464d5567de328981198c1192
chlee <z000000375@wemakeprice.com>
2023-02-20 15:51:16 +0900

    Initial Commit

---
+++ SeatingChart/SeatingChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeatingChartApp.swift
+//  SeatingChart
+//
+//  Created by gzonelee on 2023/02/20.
+//
+
+import SwiftUI
+
+@main
+struct SeatingChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
